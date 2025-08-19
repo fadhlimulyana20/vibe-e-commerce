be0db23d29dc374c5a2884e938aed55acbb02daf
Fadhli Mulyana <baba.fadhli@gmail.com>
2025-08-19 11:24:58 +0700

    ci: add netlify plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@netlify/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
   server: {
     proxy: {
   '/api': process.env.BACKEND_API_URL || 'http://localhost:3000',
