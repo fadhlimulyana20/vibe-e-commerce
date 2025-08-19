924c1d19302b364df2d2a02d293a0c5221c71836
Fadhli Mulyana <baba.fadhli@gmail.com>
2025-08-19 10:28:25 +0700

    feat: add remote db connection

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,10 +2,15 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: '', // ganti sesuai kebutuhan
-      database: 'toko-online-db-1',
+      // host: '127.0.0.1',
+      // user: 'root',
+      // password: '', // ganti sesuai kebutuhan
+      // database: 'toko-online-db-1',
+      // port: 3306 // ganti sesuai port MySQL jika berbeda
+      host: 'bn8mw3fhnkqmwgtwxiak-mysql.services.clever-cloud.com',
+      user: 'urqov1gdsrgq4r0y',
+      password: 'IumBahlcVvzhJPHn2B6d', // ganti sesuai kebutuhan
+      database: 'bn8mw3fhnkqmwgtwxiak',
       port: 3306 // ganti sesuai port MySQL jika berbeda
     },
     migrations: {
